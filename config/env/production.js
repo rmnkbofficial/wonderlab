1bcb387c12f589adafb0a55387eba32cfd0cbf07
Rama Nakib <ramalnkb@gmail.com>
2017-12-08 17:48:03 +0000

    lol

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 module.exports = {
-    db_uri: 'mongodb://rmnkbofficial-wonderlab-5711845:27017/db',
+    db_uri: 'mongodb://rmnkbofficial:pink4ever@ds135196.mlab.com:35196/wonderlab',
     db: function(callback) {
         var MongoClient = require('mongodb').MongoClient;
-        MongoClient.connect('mongodb://rmnkbofficial-wonderlab-5711845:27017/db', callback);
+        MongoClient.connect('mongodb://rmnkbofficial:pink4ever@ds135196.mlab.com:35196/wonderlab', callback);
     }
 };
 
